e2828dcd38457ac8dbc21daf744f50b30039803f
raythx98 <65889977+raythx98@users.noreply.github.com>
2021-12-27 21:40:30 +0800

    Attempt Integration

--- api/index.js
+++ api/index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
+const productRoute = require("./routes/product")
+const orderRoute = require("./routes/order")
+const cartRoute = require("./routes/cart")
 
 dotenv.config();
 
@@ -16,6 +19,9 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/carts", cartRoute);
 
 app.listen(process.env.PORT || 5000, ()=>{
   console.log("Backend server is listening on port 5000")
--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,26 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-const App = () => {
-  return <Home/>;
-};
+function App() {
+  const user = true
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={user ? <Navigate replace to="/" /> : <Login/>} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+}
+
+// const App = () => {
+//   return <Home/>;
+// };
 
 export default App;
